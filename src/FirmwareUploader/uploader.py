75dd5f4fbc7cfa09c5a86be69922bcfd29858950
Josef Sikula <joza.sikula@gmail.com>
2024-11-04 23:06:29 +0100

    firmware uploader debugged

--- src/FirmwareUploader/uploader.py
+++ src/FirmwareUploader/uploader.py
@@ -46,40 +46,69 @@ if __name__ == "__main__":
                 time.sleep(0.00001)
             recByte = usartPort.read().decode()            
             received = recByte            
-            while "C" not in received:
+            while "C" not in received and "FWR" not in received:
                 print(recByte, end="")
                 if usartPort.in_waiting > 0:
                     recByte = usartPort.read(1).decode(errors="replace")                    
                     received += recByte
+           
+                
         else:
             while usartPort.in_waiting > 0:
                 toDecode = usartPort.read()
                 try:
-                    
-                    print(toDecode.decode(), end="")
+                    charDecoded = toDecode.decode()
+                    if charDecoded == 'F' and nBytes > 30000:
+                        print("available next")
+                        bufferCounter -=1
+                    print(charDecoded, end="")
+                    if charDecoded == 'C':
+                        bufferCounter = 0
+                        print("Buffer manualy nuled")
                 except:
                     print(toDecode)            
     print(nBytes)
+    
+    time.sleep(0.1)
+    while usartPort.in_waiting > 0:
+        toDecode = usartPort.read()
+        try:
+            charDecoded = toDecode.decode()
+            if charDecoded == 'F' and nBytes > 30000:
+                print("available next")
+                bufferCounter -=1
+            print(charDecoded, end="")
+            if charDecoded == 'C':
+                bufferCounter = 0
+                print("Configuration upload")
+        except:
+            print(toDecode)
+    
+    bufferCounter = 0
+    
+    nBytes = 0
                 
     for byte in firmware.defaultConfig:
+        nBytes +=1
         usartPort.write(byte)
         bufferCounter += 1
         if bufferCounter >=31:
             bufferCounter = 0
             while usartPort.in_waiting == 0:    #waiting for transmitting data via i2c
                 time.sleep(0.00001)
-            received = usartPort.read()    
-            print(received.decode(), end="")
-            while "C" not in received.decode(): # not acknowlaged with correct char
+            recByte = usartPort.read().decode()            
+            received = recByte            
+            while "C" not in received and "CFG" not in received:
+                print(recByte, end="")
                 if usartPort.in_waiting > 0:
-                    recByte = usartPort.read()
-                    print(recByte.decode(), end="")
+                    recByte = usartPort.read(1).decode(errors="replace")                    
                     received += recByte
            
     while 1:
         if usartPort.in_waiting > 0:
             print(usartPort.read().decode())
             
+print(firmware.firmware[32767])            
 
             
     
