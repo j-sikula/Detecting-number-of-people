99a95062d1d89ea747753dc3b9d5ecfde556d4c8
Josef Sikula <joza.sikula@gmail.com>
2024-11-08 10:19:56 +0100

    lib for ESP32 - VL53L7CX added

---
+++ src/ESP32-VL53L7CXA/lib/platform/platform.c
@@ -0,0 +1,211 @@
+/**
+  *
+  * Copyright (c) 2021 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+
+#include "platform.h"
+
+#include <driver/i2c.h>   
+
+
+void VL53L7CX_InitCommunication(VL53L7CX_Platform *p_platform)
+{
+	gpio_reset_pin(INT);
+    gpio_reset_pin(I2C_RST);
+    gpio_reset_pin(LPN);
+    gpio_reset_pin(PWR_EN);
+
+	gpio_set_direction(INT, GPIO_MODE_INPUT);
+    gpio_set_direction(I2C_RST, GPIO_MODE_OUTPUT);
+    gpio_set_direction(LPN, GPIO_MODE_OUTPUT);
+    gpio_set_direction(PWR_EN, GPIO_MODE_OUTPUT);
+
+    gpio_set_level(I2C_RST, 0); //no reset
+    gpio_set_level(LPN, 1);     //no LPn 
+    gpio_set_level(PWR_EN, 1);  //enabled   
+}
+
+
+uint8_t VL53L7CX_RdByte(
+		VL53L7CX_Platform *p_platform,
+		uint16_t RegisterAdress,
+		uint8_t *p_value)
+{
+	
+	
+	uint8_t status = VL53L7CX_RdMulti(p_platform, RegisterAdress, p_value, 1);
+
+	return status;
+}
+
+uint8_t VL53L7CX_WrByte(
+		VL53L7CX_Platform *p_platform,
+		uint16_t RegisterAdress,
+		uint8_t value)
+{
+	
+	uint8_t status = VL53L7CX_WrMulti(p_platform, RegisterAdress, &value, 1);
+  
+	return status;
+}
+
+uint8_t VL53L7CX_WrMulti(
+		VL53L7CX_Platform *p_platform,
+		uint16_t RegisterAdress,
+		uint8_t *p_values,
+		uint32_t size)
+{
+	uint8_t status = 255;
+
+	//https://github.com/stm32duino/VL53L7CX/blob/main/src/platform.cpp
+	
+
+	uint32_t i = 0;	
+	while (i < size) 
+	{
+		twi_start();
+		uint8_t address = (uint8_t)((p_platform->address >> 1) & 0xFF);		//tested, address 0x29 respond
+		status = twi_write((address<<1)|TWI_WRITE);
+		
+
+		status = twi_write(RegisterAdress >> 8);
+		
+		status = twi_write(RegisterAdress & 0xff);
+		
+		
+		for (uint8_t j = 0; j < TWI_BUFFER_SIZE; j++)	// used for filling buffer with data
+		{
+			if (twi_write(*(p_values + i)) != 0) {
+				uart_putc(i+'0');
+				uart_puts("NACK write TWI\n");
+				delay(100);
+				return 1;
+			} else {
+				i++;
+				if (i >= size){
+					twi_stop();
+					return 0;
+				}
+			}
+		}
+		
+    }
+	twi_stop();
+	status = 0;	
+   
+		/* Need to be implemented by customer. This function returns 0 if OK */
+
+	return status;
+}
+
+uint8_t VL53L7CX_RdMulti(
+		VL53L7CX_Platform *p_platform,
+		uint16_t RegisterAdress,
+		uint8_t *p_values,
+		uint32_t size)
+{
+	uint8_t status = 255;
+
+	//https://github.com/stm32duino/VL53L7CX/blob/main/src/platform.cpp
+
+	uint32_t i = 0;
+	while (i < size) 
+	{
+		twi_start();
+		uint8_t address = (uint8_t)((p_platform->address >> 1) & 0xFF);
+		status = twi_write(address<<1|TWI_WRITE);
+
+		status = twi_write(RegisterAdress >> 8);
+		status = twi_write(RegisterAdress & 0xff);
+		
+		twi_stop();
+		twi_start();
+
+		status = twi_write(address<<1|TWI_READ);
+		
+		for (uint8_t j = 0; j < TWI_BUFFER_SIZE; j++)	// used for filling buffer with data
+		{
+			
+			if(size-i <= 1){	//last byte with NACK
+				p_values[i] = twi_read(TWI_NACK);
+				twi_stop();
+				return 0;
+			} 
+			else
+			{
+				p_values[i] = twi_read(TWI_ACK);
+        		i++;
+			}
+		}
+    }
+	twi_stop();
+	status = 0;	
+
+	
+	
+	
+	return status;
+}
+
+uint8_t VL53L7CX_Reset_Sensor(
+		VL53L7CX_Platform *p_platform)
+{
+	uint8_t status = 0;
+	
+	/* (Optional) Need to be implemented by customer. This function returns 0 if OK */
+	
+	/* Set pin LPN to LOW */
+	GPIO_write_low(&PORTD, LPN);
+	/* Set pin AVDD to LOW */
+	GPIO_write_low(&PORTD, PWR_EN);
+	/* Set pin VDDIO  to LOW */
+	VL53L7CX_WaitMs(p_platform, 100);
+
+	/* Set pin LPN of to HIGH */
+	GPIO_write_high(&PORTD, LPN);
+	/* Set pin AVDD of to HIGH */
+	GPIO_write_high(&PORTD, PWR_EN);
+	/* Set pin VDDIO of  to HIGH */
+	VL53L7CX_WaitMs(p_platform, 100);
+
+	return status;
+}
+
+void VL53L7CX_SwapBuffer(
+		uint8_t 		*buffer,
+		uint16_t 	 	 size)
+{
+	uint32_t i, tmp;
+	
+	/* Example of possible implementation using <string.h> */
+	for(i = 0; i < size; i = i + 4) 
+	{
+		tmp = (
+		  buffer[i]<<24)
+		|(buffer[i+1]<<16)
+		|(buffer[i+2]<<8)
+		|(buffer[i+3]);
+		
+		memcpy(&(buffer[i]), &tmp, 4);
+	}
+}	
+
+uint8_t VL53L7CX_WaitMs(
+		VL53L7CX_Platform *p_platform,
+		uint32_t TimeMs)
+{
+	
+	/* Need to be implemented by customer. This function returns 0 if OK */
+	vTaskDelay(TimeMs / portTICK_PERIOD_MS);
+	
+	return 0;
+}
+
+
--- src/ESP32-VL53L7CXA/src/main.c
+++ src/ESP32-VL53L7CXA/src/main.c
@@ -15,7 +15,8 @@
 #include <freertos/task.h>
 #include <esp_log.h>            // ESP_LOG/E/W/I functions
 #include <driver/i2c.h>         // Inter-Integrated Circuit driver
-
+#include "platform.h"
+#include "vl53l7cx_api.h"
 
 /*-----------------------------------------------------------*/
 #define I2C_MASTER_SDA_IO 21
@@ -23,10 +24,7 @@
 #define I2C_MASTER_FREQ_HZ 100000
 #define BUILT_IN_LED 2
 
-#define INT     12  //D13   //input active low - view page 2 @ https://www.st.com/content/ccc/resource/technical/layouts_and_diagrams/schematic_pack/group2/81/6d/6a/e1/a3/77/40/41/X-NUCLEO-53L7A1_SCHEMATIC/files/x-nucleo-53l7a1-schematic.pdf/jcr:content/translations/en.x-nucleo-53l7a1-schematic.pdf
-#define I2C_RST 4   //D12   //output  active HIGH
-#define LPN     16  //D11   //output  active LOW
-#define PWR_EN  17  //D10   //output  active HIGH
+
 
 /*-----------------------------------------------------------*/
 // Used function(s)
@@ -40,21 +38,45 @@ void vTaskLoop();
 void app_main(void)
 {
     //Set GPIO
-    gpio_reset_pin(BUILT_IN_LED);
-    gpio_reset_pin(INT);
-    gpio_reset_pin(I2C_RST);
-    gpio_reset_pin(LPN);
-    gpio_reset_pin(PWR_EN);
-    
+    gpio_reset_pin(BUILT_IN_LED); 
     gpio_set_direction(BUILT_IN_LED, GPIO_MODE_OUTPUT);
-    gpio_set_direction(INT, GPIO_MODE_INPUT);
-    gpio_set_direction(I2C_RST, GPIO_MODE_OUTPUT);
-    gpio_set_direction(LPN, GPIO_MODE_OUTPUT);
-    gpio_set_direction(PWR_EN, GPIO_MODE_OUTPUT);
 
-    gpio_set_level(I2C_RST, 0); //no reset
-    gpio_set_level(LPN, 1);     //no LPn 
-    gpio_set_level(PWR_EN, 1);  //enabled           
+    /*********************************/
+	/*   VL53L7CX ranging variables  */
+	/*********************************/
+
+	uint8_t 				status, loop, isAlive, isReady, i;
+	VL53L7CX_Configuration 	Dev;			/* Sensor configuration */
+	VL53L7CX_ResultsData 	Results;		/* Results data from VL53L7CX */
+
+
+	/*********************************/
+	/*      Customer platform        */
+	/*********************************/
+
+	/* Fill the platform structure with customer's implementation. For this
+	* example, only the I2C address is used.
+	*/
+	Dev.platform.address = VL53L7CX_DEFAULT_I2C_ADDRESS;
+
+	/* (Optional) Reset sensor toggling PINs (see platform, not in API) */
+	//VL53L7CX_Reset_Sensor(&(Dev.platform));
+
+	/* (Optional) Set a new I2C address if the wanted address is different
+	* from the default one (filled with 0x20 for this example).
+	*/
+	//status = vl53l7cx_set_i2c_address(&Dev, 0x20);
+
+	
+	/*********************************/
+	/*   Power on sensor and init    */
+	/*********************************/
+
+    VL53L7CX_InitCommunication(&(Dev.platform));
+
+	/* (Optional) Check if there is a VL53L7CX sensor connected */
+	status = vl53l7cx_is_alive(&Dev, &isAlive);
+            
 
 
     ESP_LOGI("setup", "i2c scan application");
@@ -86,32 +108,32 @@ void vTaskI2CScanner()
     uint8_t devices_found = 0;
 
     ESP_LOGI("i2c", "scanning the bus...");
-    for (uint8_t sla = 8; sla < 119; sla++) {
+    uint8_t sla = 29;
         // Create the i2c commands list
-        i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
 
-        // Queue a "START" signal to a list
-        i2c_master_start(cmd);
+    // Queue a "START" signal to a list
+    i2c_master_start(cmd);
 
-        // Slave address to write data
-        i2c_master_write_byte(cmd, (sla<<1) | I2C_MASTER_WRITE, true);
+    // Slave address to write data
+    i2c_master_write_byte(cmd, (sla<<1) | I2C_MASTER_WRITE, true);
 
-        // Queue a "STOP" signal to a list
-        i2c_master_stop(cmd);
+    // Queue a "STOP" signal to a list
+    i2c_master_stop(cmd);
 
-        ESP_LOGI("i2c", "0x%02x", sla);
+    ESP_LOGI("i2c", "0x%02x", sla);
 
-        // Send all the queued commands on the I2C bus, in master mode
-        if (i2c_master_cmd_begin(I2C_NUM_0, cmd, (1000/portTICK_PERIOD_MS)) == ESP_OK) {
-            ESP_LOGI("i2c", "found device with address 0x%02x", sla);
-            devices_found++;
-        }
-
-        // Free the I2C commands list
-        i2c_cmd_link_delete(cmd);
-
-        vTaskDelay(25 / portTICK_PERIOD_MS);  // 25 milliseconds
+    // Send all the queued commands on the I2C bus, in master mode
+    if (i2c_master_cmd_begin(I2C_NUM_0, cmd, (1000/portTICK_PERIOD_MS)) == ESP_OK) {
+        ESP_LOGI("i2c", "found device with address 0x%02x", sla);
+        devices_found++;
     }
+
+    // Free the I2C commands list
+    i2c_cmd_link_delete(cmd);
+
+    vTaskDelay(25 / portTICK_PERIOD_MS);  // 25 milliseconds
+    
     ESP_LOGI("i2c", "...scan completed!");
     ESP_LOGI("i2c", "%d device(s) found", devices_found);
 
