11aeab74ea7c884cfebb3be5477f4fcdba04d312
Josef Sikula <joza.sikula@gmail.com>
2024-11-18 16:39:28 +0100

    created guitemplate for showing results from sensor, only showing available ports

---
+++ src/sensor_gui/lib/main.dart
@@ -0,0 +1,134 @@
+
+/*
+MIT License
+
+Copyright (c) 2020 J-P Nurmi <jpnurmi@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+///
+import 'package:flutter/material.dart';
+import 'package:flutter_libserialport/flutter_libserialport.dart';
+
+void main() => runApp(const ExampleApp());
+
+class ExampleApp extends StatefulWidget {
+  const ExampleApp({super.key});
+
+  @override
+  ExampleAppState createState() => ExampleAppState();
+}
+
+extension IntToString on int {
+  String toHex() => '0x${toRadixString(16)}';
+  String toPadded([int width = 3]) => toString().padLeft(width, '0');
+  String toTransport() {
+    switch (this) {
+      case SerialPortTransport.usb:
+        return 'USB';
+      case SerialPortTransport.bluetooth:
+        return 'Bluetooth';
+      case SerialPortTransport.native:
+        return 'Native';
+      default:
+        return 'Unknown';
+    }
+  }
+}
+
+class ExampleAppState extends State<ExampleApp> {
+  var availablePorts = [];
+  final ScrollController _scrollController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    initPorts();
+  }
+
+  void initPorts() {
+    setState(() => availablePorts = SerialPort.availablePorts);
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter Serial Port example'),
+        ),
+        body: Scrollbar(
+          controller: _scrollController,
+          child: ListView(
+            controller: _scrollController,
+            children: [
+              for (final address in availablePorts)
+                Builder(builder: (context) {
+                  final port = SerialPort(address);
+                  return ExpansionTile(
+                    title: Text(address),
+                    children: [
+                      CardListTile('Description', port.description),
+                      CardListTile('Transport', port.transport.toTransport()),
+                      CardListTile('USB Bus', port.busNumber?.toPadded()),
+                      CardListTile('USB Device', port.deviceNumber?.toPadded()),
+                      CardListTile('Vendor ID', port.vendorId?.toHex()),
+                      CardListTile('Product ID', port.productId?.toHex()),
+                      CardListTile('Manufacturer', port.manufacturer),
+                      CardListTile('Product Name', port.productName),
+                      CardListTile('Serial Number', port.serialNumber),
+                      CardListTile('MAC Address', port.macAddress),
+                    ],
+                  );
+                }),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: initPorts,
+          child: const Icon(Icons.refresh),
+        ),
+      ),
+    );
+  }
+}
+
+class CardListTile extends StatelessWidget {
+  final String name;
+  final String? value;
+
+  const CardListTile(this.name, this.value, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        title: Text(value ?? 'N/A'),
+        subtitle: Text(name),
+      ),
+    );
+  }
+}
