9303c488125553a60bb8c369225ba15951620ca5
Josef Sikula <joza.sikula@gmail.com>
2024-12-02 16:10:02 +0100

    Serial monitor with showing last 30 lines

--- src/sensor_gui/lib/serial_monitor.dart
+++ src/sensor_gui/lib/serial_monitor.dart
@@ -17,6 +17,7 @@ class SerialMonitorState extends State<SerialMonitor> {
   bool startListening = false;
   StreamSubscription<String>? receivedDataSubscription;
   final ScrollController _scrollController = ScrollController();
+  final int maxLines = 30;  // Maximum number of lines to display
 
   void clearDisplayText() {
     setState(() {
@@ -42,30 +43,47 @@ class SerialMonitorState extends State<SerialMonitor> {
   }
 
   @override
-  Widget build(BuildContext context) {
-    if (widget.serialPortHandler != null && startListening) {
-      receivedDataSubscription =
-          widget.serialPortHandler!.receivedData!.listen((data) {
-        setState(() {
-          displayText = '$data\n';
-        });
-      });
-      startListening = false;
-    }
+  Widget build(BuildContext context) {     
     return Scrollbar(
       controller: _scrollController,
       child: SingleChildScrollView(
         controller: _scrollController,
-        child: Column(
-          children: [
-            const Text('Serial Monitor'),
-            if (widget.serialPortHandler != null)
-              Text('Baud Rate: ${widget.serialPortHandler!.baudRate}'),
-            const Text('Data:'),
-            Center(
-              child: Text(displayText),
-            ),
-          ],
+        clipBehavior: Clip.antiAlias,
+        child: Center(
+          child: widget.serialPortHandler != null
+              ? StreamBuilder<String>(
+                  stream: widget.serialPortHandler!.receivedData!,
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const CircularProgressIndicator(); // Display a loading indicator when waiting for data.
+                    } else if (snapshot.hasError) {
+                      return Text(
+                          'Error: ${snapshot.error}'); // Display an error message if an error occurs.
+                    } else if (snapshot.hasData) {
+                      displayText += '${snapshot.data}'; // Append new data to displayText.
+                      List<String> lines = displayText.split('\n');
+                      if (lines.length >maxLines) {
+                        lines = lines.sublist(lines.length - maxLines);
+                      }
+                      displayText = lines.join('\n');
+                      WidgetsBinding.instance.addPostFrameCallback((_) {
+                        _scrollController.animateTo(
+                          _scrollController.position.maxScrollExtent,
+                          duration: const Duration(milliseconds: 300),
+                          curve: Curves.easeOut,
+                        );
+                      });
+                      return Text(
+                        displayText,
+                        style: const TextStyle(fontSize: 14),
+                      ); // Display the accumulated data.
+                    } else {
+                      return const Text(
+                          'No data available'); // Display a message when no data is available.
+                    }
+                  },
+                )
+              : const Text("No serial port handler found!"),
         ),
       ),
     );
