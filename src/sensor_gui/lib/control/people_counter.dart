895690f9eea70ed4fcf2a47caf872dd4251a4bcc
Josef Sikula <joza.sikula@gmail.com>
2024-12-19 10:41:40 +0100

    algorithm (zone active as multiple pixels above treshold) for people counting debugged

--- src/sensor_gui/lib/control/people_counter.dart
+++ src/sensor_gui/lib/control/people_counter.dart
@@ -1,6 +1,7 @@
 import 'dart:developer';
 
 import 'package:sensor_gui/control/data_decoder.dart';
+
 import 'package:sensor_gui/control/google_sheets_api.dart';
 
 class PeopleCounter {
@@ -8,7 +9,15 @@ class PeopleCounter {
   int lastPosition = 0;
   int peopleCount = 0;
   final int heightTreshold = 1000;
-  final int nZones = 4;
+
+  /// Number of pixels that must be higer than the treshold
+  final int nPixelsToActivateZone = 5;
+  static const int nZones = 4;
+
+  List<bool> zoneEntered = List.filled(nZones, false);
+  List<bool> zoneExited = List.filled(nZones, false);
+  int? positionEntered;
+
   List<int> background = List.filled(64, 0);
 
   PeopleCounter(this.apiPeopleCounter);
@@ -20,56 +29,71 @@ class PeopleCounter {
     }
     List<int> heightData = List.generate(background.length,
         (index) => background[index] - measurement.data[index]);
-    LocalMaximum localMaximum = maxAndPosition(heightData);
-    int currentZone = localMaximum.getZone(nZones);
-    if (localMaximum.value > heightTreshold) {
-      if (lastPosition == 1 && currentZone == 0) {
-        peopleCount--;
-        log('People count decremented: $peopleCount people');
-        apiPeopleCounter.appendDataRow([DateTime.now().toIso8601String(), peopleCount.toString()]);
+
+    for (int i = 0; i < nZones; i++) {
+      int nPixelsAboveTreshold = 0;
+      for (int j = 0; j < 64 / nZones; j++) {
+        if (heightData[i * 64 ~/ nZones + j] > heightTreshold) {
+          nPixelsAboveTreshold++;
+        }
       }
-      if (lastPosition == nZones - 1 && currentZone == nZones) {
-        peopleCount++;
-        log('People count incremented: $peopleCount people');
-        apiPeopleCounter.appendDataRow([DateTime.now().toIso8601String(), peopleCount.toString()]);
+      // Check if the zone is entered or exited
+      if (nPixelsAboveTreshold > nPixelsToActivateZone) {
+        zoneEntered[i] = true;
+        zoneExited[i] = false;
+        if (positionEntered == null && (i == 0 || i == nZones - 1)) {
+          positionEntered = i;
+        }
+      } else {
+        // If the zone is exited, the zone is exited only if the zone was entered
+        if (zoneEntered[i]) {
+          zoneExited[i] = true;
+        }
+
+        // If all the zones are exited, the people count is updated
+        if (zoneExited.every((element) => element)) {
+          if (positionEntered == 0) {
+            peopleCount--;
+            log('People count decremented: $peopleCount people');
+            apiPeopleCounter.appendDataRow(
+                [DateTime.now().toIso8601String(), peopleCount.toString()]);
+          }
+          if (positionEntered == nZones - 1) {
+            peopleCount++;
+            log('People count incremented: $peopleCount people');
+            apiPeopleCounter.appendDataRow(
+                [DateTime.now().toIso8601String(), peopleCount.toString()]);
+          }
+          positionEntered = null;
+          zoneEntered = List.filled(nZones, false);
+          zoneExited = List.filled(nZones, false);
+        } else {
+          // when person exits in position entered
+          if (zoneExited.equals(zoneEntered)) {
+            positionEntered = null;
+            zoneEntered = List.filled(nZones, false);
+            zoneExited = List.filled(nZones, false);
+          }
+        }
       }
-      lastPosition = currentZone;
     }
 
-    if (min(heightData) < 0) {
-      //return [];
-    }
     return heightData;
-  }
-
-  LocalMaximum maxAndPosition(List<int> sublist) {
-    int maxValue = 0;
-    int position = 0;
-    for (int i = 0; i < sublist.length; i++) {
-      if (sublist[i] > maxValue) {
-        maxValue = sublist[i];
-        position = i;
-      }
-    }
-    return LocalMaximum(maxValue, position);
-  }
-
-  int max(List<int> sublist) {
-    return maxAndPosition(sublist).value;
-  }
-
-  int min(List<int> sublist) {
-    int minValue = 0;
-    for (int x in sublist) {
-      if (x < minValue) {
-        minValue = x;
-      }
-    }
-    return minValue;
   }
 
   void setSurface(Measurement measurement) {
     background = measurement.data;
+  }
+}
+
+extension on List<bool> {
+  bool equals(List<bool> zoneEntered) {
+    for (int i = 0; i < this.length; i++) {
+      if (this[i] != zoneEntered[i]) {
+        return false;
+      }
+    }
+    return true;
   }
 }
 
